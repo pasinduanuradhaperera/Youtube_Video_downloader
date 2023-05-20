fc3b8512e770e8f51ca1bc034aa81cab86a28571
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2023-05-20 12:04:22 +0530

    Update main.py
    comments

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from pytube import YouTube as Y #imoprt pytube
 from sys import argv
 
-link = argv[1]
+link = argv[1] #assign the agrument to link variable 
 yt = Y(link)
-print("Title: ",yt.title)
-print("Vievs: ", yt.views)
+print("Title: ",yt.title) #display title 
+print("Vievs: ", yt.views) #display views 
 
-yD = yt.streams.get_highest_resolution()
+yD = yt.streams.get_highest_resolution() 
 yD.download('./downloads') #whatever you like to save downloaded video
 
 
