a09e06e6a314160dfb2c598e704a6cbb33e15dd7
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2023-07-27 21:23:16 +0530

    delete video

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from pytube import YouTube as Y #imoprt pytube
+from pytube import YouTube as Y  # import pytube
 from sys import argv
 
-link = argv[1] #assign the agrument to link variable 
+link = argv[1]  # assign the argument to link variable
 yt = Y(link)
-print("Title: ",yt.title) #display title 
-print("Vievs: ", yt.views) #display views 
+print("Title: ", yt.title)  # display title
+print("Vievs: ", yt.views)  # display views
 
 yD = yt.streams.get_highest_resolution() 
-yD.download('./downloads') #whatever you like to save downloaded video
+yD.download('./downloads')  # whatever you like to save downloaded video
 
 
